2f6d72e828aa462b7b857285158a3080f83f688d
Billy <hchoi92@hotmail.com>
2018-06-10 11:39:52 -0700

    commiting all major changes

---
+++ JS/main.js
@@ -0,0 +1,60 @@
+$('#jumbotexth1').delay(1000).animate({
+    top: '+50',
+
+}, 0).animate({
+  opacity:'1',
+  top: '0'
+},1000);
+
+$('#jumbotexth2').delay(2000).animate({
+
+    top: '+50',
+
+}, 0).animate({
+  opacity:'1',
+  top: '0'
+},1000);
+
+
+$('#jumbo').animate({
+  width: '0'
+},0).animate({
+  opacity: '1',
+  width: '100%'
+},1000)
+
+
+$('#jumboButtonBox').delay(3000).animate({
+  width: '0'
+},0).animate({
+  opacity: '1',
+  width: '100%'
+},1000)
+
+$('#jumbo-button-1').delay(3500).animate({
+    top: '+50',
+}, 0).animate({
+  opacity:'1',
+  top: '0'
+},1000);
+
+$('#jumbo-button-2').delay(3800).animate({
+    top: '+50',
+}, 0).animate({
+  opacity:'1',
+  top: '0'
+},1000)
+
+$('#jumbo-button-3').delay(4100).animate({
+    top: '+50',
+}, 0).animate({
+  opacity:'1',
+  top: '0'
+},1000)
+
+$('#jumbo-button-4').delay(4400).animate({
+    top: '+50',
+}, 0).animate({
+  opacity:'1',
+  top: '0'
+},1000)
