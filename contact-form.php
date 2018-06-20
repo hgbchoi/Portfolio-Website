3fe924a69a789a79b37aa64a52566d481281836a
Billy <hchoi92@hotmail.com>
2018-06-20 00:29:39 -0700

    added favicon, loading screen, some responsiveness changes

--- contact-form.php
+++ contact-form.php
@@ -5,14 +5,14 @@ if (isset($_POST['send-mail'])){
   $name = $_POST['name'];
   $subject = $_POST['email'];
   $mailFrom = $_POST['subject'];
-  $message = $_POST['messasge'];
+  $message = $_POST['message'];
 
-  $mailTo = "hchoi92@hotmail.com";
+  $mailTo = "hchoi92@billychoi.com";
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
   mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.html");
+  header("Location: index.php?mailsent");
 
 }
 
